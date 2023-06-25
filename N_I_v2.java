2ff246c55b07cb86707eec966493f385e1d59e59
mateu <mateusz08.zajaczkowski@gmail.com>
2023-06-25 23:02:25 +0200

    Revert "Wypisanie instrukcji warunkowych "if, else""
    This reverts commit 6d0b5b6a6b61e1db803cc6476c292669b8558bae.

--- N_I_v2.java
+++ N_I_v2.java
@@ -2,14 +2,6 @@ public class N_I_v2 {
     public static void main(String[] args) {
         int liczba = 1;
         int liczba_2 = 2;
-
-        if(liczba + liczba_2 == 5)
-        {
-            System.out.println("Nieprawidłowy wynik!");
-        }
-        else
-        {
-            System.out.println("gut");
-        }
+        System.out.println(liczba + liczba_2);
     }
 }
