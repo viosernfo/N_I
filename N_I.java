bff3a7ff31ab430f29eba69259c86a7bea9770b4
mateu <mateusz08.zajaczkowski@gmail.com>
2023-06-25 22:45:15 +0200

    Utworzenie funkcji main Wypisanie do konsoli tekstu

--- N_I.java
+++ N_I.java
@@ -1,2 +1,7 @@
 public class N_I {
+    public static void main(String[] args) {
+        System.out.println("ciapki");
+        System.out.println("qwerty");
+        System.out.println("uiop");
+    }
 }
