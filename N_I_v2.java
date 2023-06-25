6d0b5b6a6b61e1db803cc6476c292669b8558bae
mateu <mateusz08.zajaczkowski@gmail.com>
2023-06-25 22:56:35 +0200

    Wypisanie instrukcji warunkowych "if, else"

--- N_I_v2.java
+++ N_I_v2.java
@@ -2,6 +2,14 @@ public class N_I_v2 {
     public static void main(String[] args) {
         int liczba = 1;
         int liczba_2 = 2;
-        System.out.println(liczba + liczba_2);
+
+        if(liczba + liczba_2 == 5)
+        {
+            System.out.println("Nieprawidłowy wynik!");
+        }
+        else
+        {
+            System.out.println("gut");
+        }
     }
 }
